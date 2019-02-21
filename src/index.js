391886e65b034670e1469fdbae8870ede0e0b736
MxWoodie <maxswation@gmail.com>
2019-02-21 22:54:54 +0300

    feat: getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+
+  for(let i=0;i<preferences.length;i++){
+
+    let firstPreference = preferences[i];
+    let secondPreference = preferences[firstPreference-1];
+    let thirdPreference = preferences[secondPreference-1];
+
+    if(i === thirdPreference-1 && firstPreference !== secondPreference && secondPreference !== thirdPreference){
+      count++;
+    }
+  }
+
+  return count/3;
 };
